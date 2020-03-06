9671f8cb193670d063acc90ff8b6c65dfa284ea0
Esmaralda Bjerrum <201708266@post.au.dk>
2020-03-06 13:01:43 +0100

    Add project files.

---
+++ TheDebtBook/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using TheDebtBook.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+
+namespace TheDebtBook
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
