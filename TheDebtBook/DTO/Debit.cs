f1cc887a694c15865c3e193c4f79e57269d15ffb
Sarah Krohn Fenger <au577425@uni.au.dk>
2020-03-06 13:40:58 +0100

    Added DTOs

---
+++ TheDebtBook/DTO/Debit.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TheDebtBook.DTO
+{
+   public class Debit
+   {
+      public DateTime DebitDateTime { get; set; }
+      public double DebitValue { get; set; }
+   }
+}
